67966714cd5021a1a942a4f7ed092021c1236ca2
brynjarb15 <brynjarb15@ru.is>
2017-10-02 11:13:23 +0000

    bæta við dataAnnotations

--- Models/ViewModels/MenuItemViewModel.cs
+++ Models/ViewModels/MenuItemViewModel.cs
@@ -1,15 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PizzaApi.Models.ViewModels
 {
 	public class MenuItemViewModel
 	{
+		[Required]
 		public string Name { get; set; }
-        
+		[Required]
 		public int SpicyLevel { get; set; }
-
+		[Required]
 		public string Description { get; set; }
-
-        public double Price { get; set; }
+		[Required]
+		public double Price { get; set; }
 	}
 }
--- Models/ViewModels/OrderViewModel.cs
+++ Models/ViewModels/OrderViewModel.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PizzaApi.Models.ViewModels
 {
 	public class OrderViewModel
 	{
 		public DateTime DateOfOrder { get; set; }
-        
 		public string CustomerName { get; set; }
 
 		public bool IsPickup { get; set; }
-
-        public string Address { get; set; }
-
-        public List<int> OrderItemsIds { get; set; }
+		[Required]
+		public string Address { get; set; }
+		public List<int> OrderItemsIds { get; set; }
 	}
 }
